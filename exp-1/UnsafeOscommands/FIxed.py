331576d58ad78e7da8bb96acc16ea547d748e7ac
astitav36 <148376792+astitav36@users.noreply.github.com>
2026-05-01 00:16:09 +0530

    Delete exp-1 directory

--- exp-1/UnsafeOscommands/FIxed.py
+++
@@ -1,6 +0,0 @@
-import os
-user = input("Enter your name: ")
-command = "echo Hello " + user
-os.system(command)
-
-# use input as : "filename" 
--- exp-1/UnsafeOscommands/Vuln.py
+++
@@ -1,9 +0,0 @@
-import os
-user = input("Enter your name: ")
-command = "echo Hello " + user
-os.system(command)
-
-# use input as : "filename" ; ls 
-          # or   "filename" ; whoami
-          # or   "filename" ; uname -a
-          # or   "filename" ; pwd 
